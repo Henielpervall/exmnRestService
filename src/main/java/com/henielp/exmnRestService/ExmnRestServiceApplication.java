3c6b496885748a6a0342c7866447a0d46e5f248e
henielpervall <hector.niel@gmail.com>
2022-05-11 10:36:03 -0500

    inicio examen

---
+++ src/main/java/com/henielp/exmnRestService/ExmnRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.henielp.exmnRestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExmnRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExmnRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henielp/exmnRestService/ExmnRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henielp.exmnRestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExmnRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
